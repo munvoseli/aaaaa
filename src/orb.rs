8c9153a3a8739878a49768b1b5dc194cd70c629f
munvoseli <muslinmelody@gmail.com>
2022-03-25 20:31:47 -0400

    OOORB

--- src/orb.rs
+++ src/orb.rs
@@ -32,13 +32,18 @@ impl Orb {
 			let mut orb = &mut world.orbs[i];
 			if t >= 0x90 && t < 0x98 {
 				let tf = ((t >> 2) & 1) + 1;
-				if orb.flavor != 0 && tf != orb.flavor { continue; }
-				let s = (t as i32 & 2) - 1;
-				orb.v.x = (t as i32 & 1) * -s * 32;
-				orb.v.y = ((t as i32 & 1) ^ 1) * s * 32;
-				if orb.flavor == 0 {
-					orb.flavor = tf;
-					orb.age = 0;
+				if orb.flavor == 0 || tf == orb.flavor {
+					let s = (t as i32 & 2) - 1;
+					let dx = (t as i32 & 1) * -s;
+					let dy = ((t as i32 & 1) ^ 1) * s;
+					orb.v.x = dx * 32;
+					orb.v.y = dy * 32;
+					orb.pos.subx = (128 + dx * 112) as u8;
+					orb.pos.suby = (128 + dy * 112) as u8;
+					if orb.flavor == 0 {
+						orb.flavor = tf;
+						orb.age = 0;
+					}
 				}
 			}
 			i += 1;
